75808ef80d400db7493decaadd25f3a0a3b241b7
Kostya Buzunov <buzunov.constantine@yandex.ru>
2022-12-30 15:25:23 +0300

    feat: install redux-devtools and redux-persist

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';import './index.css';
-import App from "./components/app/app"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./components/app/app";
+import { store, persistor } from "./services/reducers/index";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
