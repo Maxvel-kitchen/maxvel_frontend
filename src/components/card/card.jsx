3b549b2dd69ed24f9b02ff4971b530f73c18bca0
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-01-10 15:01:21 +0300

    feat: add buttons to the card component

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import style from "./card.module.css";
+import CardButton from "../card-button/card-button";
 
 function Card() {
   // eslint-disable-next-line no-unused-vars
@@ -9,9 +10,11 @@ function Card() {
     <li className={style.card}>
       {tag && <p className={style.tag}>New</p>}
       <div className={style.image}>Изображение</div>
-      <p className={style.title}>
-        Название блюда Lorem ipsum dolor sit
-      </p>
+      <p className={style.title}>Название блюда Lorem ipsum dolor sit</p>
+      <div className={style.container}>
+        <p className={style.price}>3500 ₽</p>
+        <CardButton />
+      </div>
     </li>
   );
 }
