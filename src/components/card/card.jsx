4c77ac5d6ce4b68f54634b6fb1522d0b37e380ad
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-01-11 17:36:44 +0300

    feat: make card component

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,19 +1,33 @@
+/* eslint-disable react/self-closing-comp */
 import { useState } from "react";
 import style from "./card.module.css";
-import CardButton from "../card-button/card-button";
+import Counter from "../counter/counter";
 
-function Card() {
-  // eslint-disable-next-line no-unused-vars
-  const [tag, setTag] = useState(true);
+function Card() {  
+  const [button, setButton] = useState(true);
+
+  function hideItem() {
+    setButton(false);
+  }
+
+  function showItem() {
+    setButton(true);
+  }
 
   return (
     <li className={style.card}>
-      {tag && <p className={style.tag}>New</p>}
-      <div className={style.image}>Изображение</div>
-      <p className={style.title}>Название блюда Lorem ipsum dolor sit</p>
+      {false && <p className={style.tag}>New</p>}
+      <div className={style.image}></div>
+      <p className={style.title}>Длинное название блюда 5</p>
       <div className={style.container}>
-        <p className={style.price}>3500 ₽</p>
-        <CardButton />
+        <p className={style.price}>3500 €</p>
+        {button ? (
+          <button onClick={hideItem} className={style.button} type="button">
+            Добавить
+          </button>
+        ) : (
+          <Counter showItem={() => showItem()} />
+        )}
       </div>
     </li>
   );
---
+++ src/components/counter/counter.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import style from "./counter.module.css";
+
+function Counter({ showItem }) {
+  const [counter, setCounter] = useState(1);
+
+  function increaseCounter() {
+    setCounter((prevValue) => prevValue + 1);
+  }
+
+  function decreaseCounter() {
+    setCounter((prevValue) => prevValue - 1);
+  }
+
+  useEffect(() => {
+    if (counter === 0) {
+      showItem();
+    }
+  }, [counter, showItem]);
+
+  return (
+    <div className={style.container}>
+      <button onClick={decreaseCounter} className={style.button} type="button">
+        -
+      </button>
+      <input
+        onChange={(e) => setCounter(Number(e.target.value))}
+        className={style.input}
+        type="text"
+        value={counter}
+      />
+      <button onClick={increaseCounter} className={style.button} type="button">
+        +
+      </button>
+    </div>
+  );
+}
+
+Counter.propTypes = {
+  showItem: PropTypes.func.isRequired,
+};
+
+export default Counter;
