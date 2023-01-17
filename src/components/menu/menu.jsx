0817a037e29e909a6b7523946f2d37297b3fa75e
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-01-17 12:10:51 +0300

    feat: make adaptive for the menu list

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,12 +1,33 @@
+/* eslint-disable prefer-const */
+import { useState, useEffect, useCallback } from "react";
 import CardList from "../card-list/card-list";
 import MenuList from "../menu-list/menu-list";
 import style from "./menu.module.css";
 
 function Menu() {
+  const [width, setWidth] = useState(undefined);
+  const [menuList, setMenuList] = useState(true);
+
+  function windowWidth() {
+    setWidth(() => window.innerWidth);
+  }
+
+  const checkWidth = useCallback(() => {
+    if (width < 768) {
+      setMenuList(false);
+    } else {
+      setMenuList(true);
+    }
+  }, [width])
+
+  useEffect(() => window.addEventListener("resize", windowWidth), []);
+
+  useEffect(() => checkWidth(), [checkWidth, width]);
+
   return (
     <main className={style.menu}>
       <div className={style.container}>
-        <MenuList />
+        {menuList && <MenuList />}
         <CardList />
       </div>
     </main>
