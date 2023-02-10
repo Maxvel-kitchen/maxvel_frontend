495ac5d07cb46169b138342dad3b5208611772bd
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2023-02-10 12:27:19 +0300

    feat: counter input for keyboard

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -5,6 +5,8 @@ import PropTypes from "prop-types";
 import {
   increase,
   decrease,
+  type,
+  returnItem,
   removeItem,
 } from "../../services/reducers/cart-slice";
 import style from "./counter.module.css";
@@ -12,15 +14,15 @@ import style from "./counter.module.css";
 function Counter({ id, amount, styles, handleDelete }) {
   const dispatch = useDispatch();
   const location = useLocation();
+
   return (
     <div className={`${style.container} ${styles}`}>
       <button
         onClick={() => {
+          dispatch(decrease(id));
           if (amount === 1 && location.pathname === "/cart") {
-            dispatch(decrease(id));
             handleDelete();
           } else {
-            dispatch(decrease(id));
             dispatch(removeItem(id));
           }
         }}
@@ -33,8 +35,14 @@ function Counter({ id, amount, styles, handleDelete }) {
         className={style.input}
         type="number"
         value={amount}
-        max="999"
-        min="0"
+        onChange={(e) => {
+          dispatch(type([id, Number(e.target.value)]));
+        }}
+        onBlur={() => {
+          if (amount === (null || "")) {
+            dispatch(returnItem(id));
+          }
+        }}
       />
       <button
         onClick={() => dispatch(increase(id))}
