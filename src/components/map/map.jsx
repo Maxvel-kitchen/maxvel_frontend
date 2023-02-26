b9109ee27d880bf1bf23cc0202707f3c4c654492
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2023-02-26 20:28:47 +0300

    fix: map, menu-list display

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -6,8 +6,6 @@ function Map() {
       <iframe
         className={style.content}
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d791.3358605266632!2d33.322777290886265!3d34.781557486838494!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14e0bb9f7cedf309%3A0xb8f923ea8f96bb9!2sMaxVel%20Kitchen!5e0!3m2!1sru!2sru!4v1677420877538!5m2!1sru!2sru"
-        width="297"
-        height="203"
         style={{ border: 0, borderRadius: "20px" }}
         allowFullScreen=""
         loading="lazy"
